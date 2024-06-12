2829db852ae07d88a4643dd933052018c91d0bad
ethnjs <168794808+ethnjs@users.noreply.github.com>
2024-06-11 18:24:31 -0700

    added js for testimonial slider and faq accordion

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// Hide Header on on scroll down
+/* -------------------------
+HIDE HEADER ON SCROLL DOWN
+------------------------- */
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
@@ -36,3 +38,59 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
+
+/* -------------------------
+SLIDER
+------------------------- */
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let numItems = items.length;
+    
+let active = 1;
+function loadShow(){
+    let stt = 0;
+    items[active].style.transform = 'none';
+    items[active].style.zIndex = numItems;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for(var i = active + 1; i < numItems; i++){
+        stt++;
+        items[i].style.transform = `translateX(${150*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = numItems - stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+    stt = 0;
+    for(var i = active - 1; i >= 0; i--){
+        stt++;
+        items[i].style.transform = `translateX(${-150*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+        items[i].style.zIndex = numItems - stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+loadShow();
+next.onclick = function(){
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+}
+
+/* -------------------------
+FAQs ACCORDION
+------------------------- */
+document.querySelectorAll('.accordion .item .question').forEach(anchor => {
+    anchor.addEventListener('click', function() {
+      const item = this.parentElement;
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        item.classList.add('active');
+      }
+    });
+  }); 
+
